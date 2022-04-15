869dca798c08230c7671c622f4d7c018110779e6
fall211 <tuukka02@gmail.com>
2022-04-15 02:31:40 -0700

    some c# stuffs

--- Program.cs
+++ Program.cs
@@ -4,9 +4,20 @@ namespace MyProgram
 {
     class MainClass
     {
-        public static void Main (string[] args)
+        static void Main ()
         {
-            Console.WriteLine("hello");
+            // variables
+            var notdefined = 100; // program will guess what type of variable it is
+            int integer = 10;
+            string sentence = "string";
+            bool yesno = true; // true/false, no caps unlike in python
+
+            Console.WriteLine("set a value for integer " + sentence);
+            integer = Convert.ToInt32(Console.ReadLine());
+            if (yesno == true){
+                Console.WriteLine(Convert.ToString(integer*notdefined));
+            }
+            
         }
     }
 }
